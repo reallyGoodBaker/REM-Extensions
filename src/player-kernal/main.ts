3dc94b536fcce741ec3c11d14bca438a8a189dae
reallyGoodBaker <79316659+reallyGoodBaker@users.noreply.github.com>
2024-06-15 05:38:28 +0800

    update

--- src/player-kernal/main.ts
+++ src/player-kernal/main.ts
@@ -3,6 +3,11 @@ import { RtAudio, RtAudioFormat } from 'audify'
 
 let pcmReciever: Socket | null = null
 let audio: RtAudio = new RtAudio()
+let renderBuffer: Buffer[] = []
+
+const sampleRate = 48000
+const channels = 2
+const bufferSize = 4096
 
 async function createAudio() {
     if (audio) {
@@ -20,24 +25,39 @@ async function createAudio() {
     audio?.openStream(
         {
             deviceId: device.id,
-            nChannels: 2,
+            nChannels: channels,
         },
         null,
         RtAudioFormat.RTAUDIO_FLOAT32,
-        48000,
-        8192,
+        sampleRate,
+        bufferSize,
         'mainStream',
         null,
         null,
     )
 
+    const writeBlank = () => {
+        const buffer = Buffer.alloc(bufferSize * channels * 4).fill(0)
+        audio.write(buffer)
+    }
+
+    audio.setFrameOutputCallback(() => {
+        const buffer = renderBuffer.shift()
+        console.log(performance.now())
+
+        buffer
+            ? audio.write(buffer)
+            : writeBlank()
+    })
+
     audio.start()
+    writeBlank()
 }
 
 whenReady(async () => {
-    pcmReciever = ipc.subscribePcm('pcm', buffer => {
+    pcmReciever = ipc.registerPcmStreamReceiver(buffer => {
         if (audio.isStreamRunning()) {
-            audio.write(buffer)
+            renderBuffer.push(buffer)
         }
     })
 
--- src/player-kernal/settings.ts
+++ src/player-kernal/settings.ts
@@ -1,7 +1,8 @@
-import { TextField } from 'extension/ui/index'
+import { TextField, NumberField } from 'extension/ui/index'
 
 export const onSetting: UIExports.OnSetting = () => {
     return [
-        TextField('写着玩的')
+        TextField('写着玩的'),
+        NumberField('也是写着玩的')
     ]
 }
